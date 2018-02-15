75e018b1ffee05c5a59b4b24d945e322adcb84e7
Matt Ricks <mgizmor37@gmail.com>
2018-02-15 12:25:49 -0700

    Current Work

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # digitalPinage
+
+digitalPinage is a play on words for Digital Signage and Raspberry Pi.
+
+The purpose of this project is to create a program which will use the Raspberry Pi Zero only for Digital Signage on a single TV.  This project was inspired by Stone Gate Center for the Arts, LLC (http://www.stonegateweddings.com).  They needed a way to dynamically play and show content on a movable TV without using USB or DVD's of any sort.  This just turned out to be one of the simplest solutions.
+
+This project should work on any computer that has Node.js installed.
+
+To install:
+Make sure Node.js and Git is installed on your computer.
+Clone this project to your computer.
+Using terminal, navigate to the digitalPinage folder
+Run "npm install" in terminal
+Run "npm start"
+
+The server should then start, and the webpage leading to the controls for the system.
--- webserver.js
+++ webserver.js
@@ -1,76 +1,71 @@
 var http = require('http').createServer(handler); //require http server, and create server with function handler()
 var fs = require('fs'); //require filesystem module
 var formidable = require('formidable');
-var displayType = 'online';
+var nowShowing = 'Default'
 
 http.listen(8080); //listen to port 8080
 
 function handler(req, res) {
-    if (req.url == '/fileupload') {
-        var form = new formidable.IncomingForm();
-        form.parse(req, function(err, fields, files) {
-            var oldpath = files.filetoupload.path;
-            var newpath = (__dirname + '/media/video.mp4');
-            fs.rename(oldpath, newpath, function(err) {
-                if (err) {
-                    res.writeHead(500, { 'Content-Type': 'text/html' });
-                    res.write('<a href = "/">Back</a>');
-                    return res.end("Unable to upload file");
+    console.log(req.url);
+    switch (req) {
+        //These are the forms
+        case '/fileupload':
+            var form = new formidable.IncomingForm();
+            form.parse(req, function(err, fields, files) {
+                var oldpath = files.filetoupload.path;
+                var newpath = (__dirname + '/media/video.mp4');
+                fs.rename(oldpath, newpath, function(err) {
+                    if (err) {
+                        res.writeHead(500, { 'Content-Type': 'text/html' });
+                        res.write('<a href = "/">Back</a>');
+                        return res.end("Unable to upload file");
+                    } else {
+                        fs.readFile(__dirname + '/public/success.html', function(err, data) {
+                            if (err) {
+                                res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
+                                return res.end("404 Not Found");
+                            }
+                            res.writeHead(200, { 'Content-Type': 'text/html' });
+                            res.write(data); //write data from index.html
+                            return res.end();
+                        });
+                    }
+                });
+            });
+            break;
+        case '/setShowing':
+            var form = new formidable.IncomingForm();
+            form.parse(req, function(err, fields, files) {
+                if (fields.vidType !== "") {
+                    nowShowing = fields.vidType;
                 } else {
-                    fs.readFile(__dirname + '/public/success.html', function(err, data) {
-                        if (err) {
-                            res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
-                            return res.end("404 Not Found");
-                        }
-                        res.writeHead(200, { 'Content-Type': 'text/html' });
-                        res.write(data); //write data from index.html
-                        return res.end();
-                    });
+                    nowShowing = "Default";
                 }
+                fs.readFile(__dirname + '/public/success.html', function(err, data) {
+                    if (err) {
+                        res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
+                        return res.end("404 Not Found");
+                    }
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write(data); //write data from index.html
+                    return res.end();
+                });
             });
-        });
-    } else if (req.url == '/setType') {
-        var form = new formidable.IncomingForm();
-        form.parse(req, function(err, fields, files) {
-            displayType = fields.vidType
-            fs.readFile(__dirname + '/public/index.html', function(err, data) {
+            break;
+            //This retrieves the videos which display
+        case '/getVideo':
+            fs.readFile(__dirname + "/media/video.mp4", function(err, data) {
                 if (err) {
                     res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
                     return res.end("404 Not Found");
                 }
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.write(data); //write data from index.html
-                res.write(displayType);
-                return res.end();
-            });
-        });
-    } else if (req.url == '/ajax_info.txt') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(displayType); //write data from index.html
-        return res.end();
-    } else if (req.url == '/player') {
-        if (displayType === 'Online') {
-            fs.readFile(__dirname + '/public/youtube.html', function(err, data) {
-                if (err) {
-                    res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
-                    return res.end("404 Not Found");
-                }
-                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.writeHead(200, { 'Content-Type': 'video/mp4' });
                 res.write(data); //write data from index.html
                 return res.end();
             });
-
-        } else if (displayType === 'Local') {
-            fs.readFile(__dirname + '/public/localVideo.html', function(err, data) {
-                if (err) {
-                    res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
-                    return res.end("404 Not Found");
-                }
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.write(data); //write data from index.html
-                return res.end();
-            });
-        } else { //It's a slide show
+            break;
+            //This serves up the video player for the actual playing of the slide show
+        case '/player':
             fs.readFile(__dirname + '/public/slideshow.html', function(err, data) {
                 if (err) {
                     res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
@@ -80,27 +75,50 @@ function handler(req, res) {
                 res.write(data); //write data from index.html
                 return res.end();
             });
-        }
-    } else if (req.url == '/getVideo') {
-        fs.readFile(__dirname + "/media/video.mp4", function(err, data) {
-            if (err) {
-                res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
-                return res.end("404 Not Found");
-            }
-            res.writeHead(200, { 'Content-Type': 'video/mp4' });
-            res.write(data); //write data from index.html
-            return res.end();
-        });
-    } else {
-        fs.readFile(__dirname + '/public/index.html', function(err, data) {
-            if (err) {
-                res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
-                return res.end("404 Not Found");
-            }
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data); //write data from index.html
-            res.write(displayType);
-            return res.end();
-        });
+            break;
+            //These are the actual web pages that are displayed to the user
+        case '/termsconditions':
+            fs.readFile(__dirname + '/public/termsconditions.html', function(err, data) {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(data);
+                return res.end();
+            });
+            break;
+        case '/construction':
+            fs.readFile(__dirname + '/public/construction.html', function(err, data) {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(data);
+                return res.end();
+            });
+            break;
+        case "/index": //Just for the index/ home page
+        case '/':
+        case '':
+            fs.readFile(__dirname + '/public/index.html', function(err, data) {
+                if (err) {
+                    res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
+                    return res.end("404 Not Found");
+                }
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(data); //write data from index.html
+                return res.end();
+            });
+            break;
+            //For resources
+        case RegExp('/resources/*'):
+            fs.readFile(__dirname + req.url, function(err, data) {
+                if (err) {
+                    res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
+                    return res.end("404 Not Found");
+                }
+                res.writeHead(200, { 'Content-Type': 'text/css' });
+                res.write(data); //write data from index.html
+                return res.end();
+            });
+            break;
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
+            return res.end("404 Not Found");
+            break;
     }
 };
